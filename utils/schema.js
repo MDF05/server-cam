b4907eac77fc0cc4b4c828896179ef2d44bda4cb
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-23 19:45:49 +0700

    selamat datang lagi

--- utils/schema.js
+++ utils/schema.js
@@ -6,11 +6,21 @@ const schema = { video: String };
 
 const model = mongoose.model('prankCamera', schema)
 
-mongoose.connect("mongodb://https://server-cam.vercel.app:27017", {
-    autoIndex: true,
-    keepAlive: true,
-    keepAliveInitialDelay: 300000
-});
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, {
+        autoIndex: true,
+        keepAlive: true,
+        keepAliveInitialDelay: 300000
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Lakukan operasi database lainnya
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
+
 
 // (async() => {
 //     await model.insertMany({ video: 'asu' })
