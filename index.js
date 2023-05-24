88ed0ce690d6129f14c9b727d1a5074d5fb89779
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-24 21:11:04 +0700

    harus selamat dunia akhirat

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/upload', upload.single('video'), async(req, res) => {
     newVideo.video.contentType = req.file.mimetype;
 
     try {
-        await model.create(newVideo)
+        await newVideo.save()
         res.json({ status: 200, video: 'updloaded' })
     } catch (err) {
         res.json({ status: 500, video: 'gagal diupload' })
