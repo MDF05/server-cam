a7163d3803ee747c7e0bf19b64b6d3857c49cbc2
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 10:34:39 +0700

    lagi bang

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const app = express();
-const corsOption = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-}
-
-app.use(cors(corsOption))
-
 const path = require('path')
 const mongoose = require('mongoose');
 const { Model } = require('./utils/schema')
@@ -27,7 +18,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+const corsOption = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
 
+app.use(cors(corsOption))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
