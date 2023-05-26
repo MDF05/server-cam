3d6aec674a8066d71b448bf33d7b547c62a60eab
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 22:54:08 +0700

    selamat pagi cikgu besar

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', upload.single('file'), async(req, res) => {
-    return res.json({ data: req.file, status: 200, pesan: 'ok' })
+    return res.json({ status: 200, pesan: 'ok' })
 });
 
 app.get('/isi', async(req, res) => {
