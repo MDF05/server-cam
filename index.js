c23d04b017378fec00f5ed920dc3a0b0616fb4cb
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 09:34:55 +0700

    ayo lebih keras lagi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const { Model } = require('./utils/schema')
 const cors = require('cors')
 const multer = require('multer');
-const upload = multer();
+const upload = multer({ dest: './public/uploads/' });
 const bodyParser = require('body-parser')
 
 const corsOption = {
