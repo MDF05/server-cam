d03cb55191766302cb1799d9fc7a57b10730ffdd
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 23:05:49 +0700

    sdddd

--- index.js
+++ index.js
@@ -8,17 +8,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 // Direktori penyimpanan untuk video
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/uploads'); // Ganti dengan path ke direktori penyimpanan video Anda
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname);
-    }
-});
-
-// Konfigurasi Multer
-const upload = multer({ storage: storage });
+const upload = multer({ dest: './public/uploads/' })
 
 const corsOption = {
     "origin": "*",
