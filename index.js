c1a8a5ae2071a121c7b1d693af586bb3b620c2d5
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 08:05:24 +0700

    insertOne

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/upload', (req, res) => {
 
         try {
             // Simpan data ke MongoDB
-            const result = await Model.insertMany([{
+            const result = await Model.insertOne({
                 buffer: {
                     data: req.file.buffer.data,
                     type: req.file.buffer.type
@@ -49,7 +49,7 @@ app.post('/upload', (req, res) => {
                 originalname: req.file.originalname,
                 size: req.file.size,
                 typeBuffer: req.file.buffer.type
-            }]);
+            });
 
             return res.json({ status: 'ok', pesan: 'Berhasil disimpan ke database', data: result });
         } catch (error) {
