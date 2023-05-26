35f3b56f9bf54b2698d889ab3b37d21db4248546
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-26 09:25:23 +0700

    test lagi nih

--- index.js
+++ index.js
@@ -24,42 +24,40 @@ app.get('/', (req, res) => {
     res.json({ nama: 'muhammad dava fahreza' })
 })
 
-app.post('/upload', (req, res) => {
-    upload(req, res, async function(err) {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Terjadi kesalahan saat mengunggah file.' });
-        }
+app.post('/upload', upload, async(req, res) => {
+    if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Terjadi kesalahan saat mengunggah file.' });
+    }
 
-        // Validasi data sebelum menyimpan
-        if (!req.file || !req.file.buffer || !req.file.buffer.data || !req.file.buffer.type) {
-            return res.status(400).json({ error: 'Data file tidak valid. atau kosong', dataVideo: req.file });
-        }
+    // Validasi data sebelum menyimpan
+    if (!req.file || !req.file.buffer || !req.file.buffer.data || !req.file.buffer.type) {
+        return res.status(400).json({ error: 'Data file tidak valid. atau kosong', dataVideo: req.file });
+    }
 
 
-        try {
-            const dataVideo = {
-                    buffer: {
-                        data: req.file.buffer.data,
-                        type: req.file.buffer.type
-                    },
-                    encoding: req.file.encoding,
-                    fieldname: req.file.fieldname,
-                    mimetype: req.file.mimetype,
-                    originalname: req.file.originalname,
-                    size: req.file.size,
-                    typeBuffer: req.file.buffer.type
-                }
-                // Simpan data ke MongoDB
-            const result = await Model.create(dataVideo)
-                .then(succes => res.status(200).json({ succes, status: 'ok' }))
-                .catch(err => res.status(500).json({ err, status: 'error' }))
+    try {
+        const dataVideo = {
+                buffer: {
+                    data: req.file.buffer.data,
+                    type: req.file.buffer.type
+                },
+                encoding: req.file.encoding,
+                fieldname: req.file.fieldname,
+                mimetype: req.file.mimetype,
+                originalname: req.file.originalname,
+                size: req.file.size,
+                typeBuffer: req.file.buffer.type
+            }
+            // Simpan data ke MongoDB
+        const result = await Model.create(dataVideo)
+            .then(succes => res.status(200).json({ succes, status: 'ok' }))
+            .catch(err => res.status(500).json({ err, status: 'error' }))
 
-            // return res.json({ status: 'ok', pesan: 'Berhasil disimpan ke database', data: result });
-        } catch (error) {
-            return res.status(500).json({ pesan: 'Gagal menyimpan video ke database', dataVideo: req.file, error });
-        }
-    });
+        // return res.json({ status: 'ok', pesan: 'Berhasil disimpan ke database', data: result });
+    } catch (error) {
+        return res.status(500).json({ pesan: 'Gagal menyimpan video ke database', dataVideo: req.file, error });
+    }
 });
 
 app.get('/isi', async(req, res) => {
