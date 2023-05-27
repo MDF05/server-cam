e57714df6ba3a648d241ba9880325ae8fd9c2fbd
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-27 12:26:33 +0700

    iseng tapi gagal

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { GridFsStorage } = require('multer-gridfs-storage');
 
 // Konfigurasi penyimpanan GridFS
 const storage = new GridFsStorage({
-    url: "mongodb+srv://palen:ngM0BQ2TGnKtk4lC@dava.v4rbver.mongodb.net/",
+    db: dbURI,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
         return {
